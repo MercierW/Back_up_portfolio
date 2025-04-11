6b94120980b4dad2f353fab72251e14c1da834ad
William <william77220@hotmail.fr>
2025-04-11 10:14:52 +0200

    mise à jour de la branche developpeur

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ const text = `< "Hey, moi c’est William – designer UI & développeur qui jon
 Bienvenue dans mon univers, où chaque détail compte (et où le café coule à flots) !" >`;
 
 const speed = 50; // adjust typing speed
+const target = document.getElementById("presentation");
 let index = 0;
 let caract;
 let timeOutID;
 
 function typeWriter() {
-        const target = document.getElementById("presentation");
     if(index > 96 && index < 106) {
             caract = text.charAt(index)
             target.innerHTML += `<span class="bold">${caract}</span>`
             index++
             timeOutID = setTimeout(typeWriter, speed)
         }else {
-                target.innerHTML += text.charAt(index)
-                index++
-                timeOutID = setTimeout(typeWriter, speed)
+            target.innerHTML += text.charAt(index)
+            index++
+            timeOutID = setTimeout(typeWriter, speed)
         if(index >= 195) {
                 clearTimeout(timeOutID)
             }
@@ -25,25 +25,3 @@ function typeWriter() {
 
 // Start the typewriter effect when the page loads
 window.onload = typeWriter;
-
-// function typeWriter() {
-//     const target = document.getElementById("presentation");
-//     if(index === 97) {
-//         caract = text.charAt(index)
-//         target.innerHTML += `<span class="bold">${caract}`
-//         index++
-//         timeOutID = setTimeout(typeWriter, speed)
-//     }else if(index === 105) {
-//         caract = text.charAt(index)
-//         target.innerHTML += `<span class="bold">${caract}`
-//         index++
-//         timeOutID = setTimeout(typeWriter, speed)
-//     }else {
-//         target.innerHTML += text.charAt(index)
-//         index++
-//         timeOutID = setTimeout(typeWriter, speed)
-//         if(index >= 195) {
-//             clearTimeout(timeOutID)
-//         }
-//     }
-// }
