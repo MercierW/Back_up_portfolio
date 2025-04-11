89314266a0260426966133ab6eba7d1d83004d79
William <william77220@hotmail.fr>
2025-04-11 09:32:21 +0200

    deuxième commit

--- js/script.js
+++ js/script.js
@@ -1,49 +1,26 @@
 const text = `< "Hey, moi c’est William – designer UI & développeur qui jongle entre pixels et lignes de code.
-Bienvenue dans mon univers, où chaque détail compte (et où le café coule à flots) !" >`;
-
+                Bienvenue dans mon univers, où chaque détail compte (et où le café coule à flots) !" >`;
 const speed = 50; // adjust typing speed
+const target = document.getElementById("presentation");
 let index = 0;
 let caract;
 let timeOutID;
 
 function typeWriter() {
-        const target = document.getElementById("presentation");
     if(index > 96 && index < 106) {
-            caract = text.charAt(index)
-            target.innerHTML += `<span class="bold">${caract}</span>`
-            index++
-            timeOutID = setTimeout(typeWriter, speed)
-        }else {
-                target.innerHTML += text.charAt(index)
-                index++
-                timeOutID = setTimeout(typeWriter, speed)
+        caract = text.charAt(index)
+        target.innerHTML += `<span class="bold">${caract}</span>`
+        index++
+        timeOutID = setTimeout(typeWriter, speed)
+    }else {
+        target.innerHTML += text.charAt(index)
+        index++
+        timeOutID = setTimeout(typeWriter, speed)
         if(index >= 195) {
-                clearTimeout(timeOutID)
-            }
+            clearTimeout(timeOutID)
         }
     }
+}
 
 // Start the typewriter effect when the page loads
 window.onload = typeWriter;
-
-// function typeWriter() {
-//     const target = document.getElementById("presentation");
-//     if(index === 97) {
-//         caract = text.charAt(index)
-//         target.innerHTML += `<span class="bold">${caract}`
-//         index++
-//         timeOutID = setTimeout(typeWriter, speed)
-//     }else if(index === 105) {
-//         caract = text.charAt(index)
-//         target.innerHTML += `<span class="bold">${caract}`
-//         index++
-//         timeOutID = setTimeout(typeWriter, speed)
-//     }else {
-//         target.innerHTML += text.charAt(index)
-//         index++
-//         timeOutID = setTimeout(typeWriter, speed)
-//         if(index >= 195) {
-//             clearTimeout(timeOutID)
-//         }
-//     }
-// }
